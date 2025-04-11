98191eccfeea4abd3c68c47048fa7aa2f115d4b0
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-04-11 18:34:46 +0000

    auto-commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_ID = 'farcaster-frames-template';
 export const PROJECT_TITLE = "Degen Collectooooor";
 export const PROJECT_DESCRIPTION = "Collect crypto treasures with your degen hat!";
-export const SCORE_THRESHOLD = 2000;
-export const INITIAL_LIVES = 3;
+export const SCORE_THRESHOLD = 3000;
+export const INITIAL_LIVES = 5;
